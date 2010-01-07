98caa02643470151893ced64513478ad43c1f76f
yfzhu <yfzhu@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2010-01-07 05:38:13 +0000

    按TONYWEAR要求，齐程制作
    git-svn-id: http://buildserver:9880/framework@135 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

---
+++ trunk/java/src/nds/web/button/ButtonExtDetail.java
@@ -0,0 +1,40 @@
+package nds.web.button;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import nds.query.QueryEngine;
+import nds.schema.Column;
+import nds.util.WebKeys;
+
+public class ButtonExtDetail extends ButtonCommandUI_Impl{
+	protected String getHandleURL(HttpServletRequest request, Column column,int objectId){
+		Connection conn=null;
+		StringBuffer sb = new StringBuffer();
+		try{
+			QueryEngine engine=QueryEngine.getInstance();
+			conn= engine.getConnection();
+   			ArrayList params1=new ArrayList();
+   			params1.add(objectId);
+			QueryEngine.getInstance().executeStoredProcedure("HR_INSERT_EXTAPPLY", params1, false,conn);
+			sb.append(WebKeys.WEB_CONTEXT_ROOT).
+			append(WebKeys.NDS_URI+ "/object/object.jsp?table=HR_EXTAPPLY&&fixedcolumns=&id="+objectId);
+			return sb.toString();
+		}catch(Throwable th){
+		       logger.error("exception",th);
+		       return "";
+		}finally{ 
+             try{conn.close();}catch(Exception e){}
+		}		
+	}
+	/**
+	 * Popup type
+	 * @return
+	 */
+	protected String getPopupType( HttpServletRequest request, Column column, int objectId){
+		return POPUP_TARGET_LARGE_DIALOG;
+	}
+
+}
