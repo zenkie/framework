e4b18b85643f0d5c327193f594d9ab90954d1bea
zhoubing <zhoubing@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2010-08-12 09:27:39 +0000

    修改构造方法，当condition中含有“,“时则分解为多个条件，它们之间 为 或者 关系；实现功能当字段非columnlink时也可多选
    git-svn-id: http://buildserver:9880/framework@233 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/query/Expression.java
+++ trunk/java/src/nds/query/Expression.java
@@ -1,10 +1,12 @@
 package nds.query;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.*;
+
 import nds.log.Logger;
 import nds.log.LoggerManager;
 import nds.schema.TableManager;
@@ -77,6 +79,37 @@ public class Expression implements SQLCombination, Serializable{
      * @param desc
      */
     public Expression(ColumnLink cl, String condition, String desc){
+    	/**
+    	 * robin 2010-08-10 当condition中含有","时，解析为多选条件
+    	 * 及每个","隔开的都是或者关系
+    	 */
+    	if(null!=condition&&condition.contains(",")){
+    		String[] cons=condition.split(",");
+    		List<String> conls=new ArrayList<String>();
+    		for(String s:cons){
+    			if(!s.equals("")){
+    				conls.add(s);
+    			}
+    		}
+    		Expression eprs=null;
+    		for(String s1:conls){
+    			if(null==eprs){
+    				eprs=new Expression(cl,s1,desc);
+    			}else{
+    				eprs=new Expression(eprs,new Expression(cl,s1,desc),SQLCombination.SQL_OR,desc);
+    			}
+    		}
+    		if(eprs.isLeaf()){
+    			this.clink=eprs.getColumnLink();
+    			this.condition=eprs.getCondition();
+    		}
+    		this.desc=eprs.getDescription();
+    		this.exprLeft=eprs.getLeftElement();
+    		this.exprRight=eprs.getRightElement();
+    		this.isLeaf=eprs.isLeaf();
+    		this.operator=eprs.getOperator();
+    		return;
+    	}
         /**
          * yfzhu 2005-05-15 发现关于LimitValue 的字段在界面上直接输入描述选项时查询会出现错误。
          * 例如：状态字段 输入"提交" 时应该由系统自动转换为2
