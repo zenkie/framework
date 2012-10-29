48eea02fdf8060bb15274235c0c4f7ffdbc0aa35
jackrain <jackrain@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2012-10-29 06:59:35 +0000

    修改支持 JSONOBJECT 方法ACTION [ 开头
    git-svn-id: http://buildserver:9880/framework@244 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/schema/SubSystem.java
+++ trunk/java/src/nds/schema/SubSystem.java
@@ -77,9 +77,11 @@ public class SubSystem {
 	}
 	public void addWebAction(WebAction action){
 		if(action.getScript()==null) throw new nds.util.NDSRuntimeException("SubSystem webaction must have script set:"+action.getId());
+		//TODO add jsonobject to webaction supply 
+		/*
 		if(!action.getScript().startsWith("<")){
 			throw new nds.util.NDSRuntimeException("SubSystem webaction must be tree xml format(starts with '<' tag):"+action.getId());
-		}
+		}*/
 		actions.add(action);
 		tcacts.add(action);
 	}
