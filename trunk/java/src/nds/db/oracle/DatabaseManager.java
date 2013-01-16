1ac036602a7dda69aa5a248d50076b23ebdd6de9
jackrain <jackrain@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-01-16 14:19:16 +0000

    增加 oracle/DatabaseManager.java DBManager 接口实现 executeUpdate 支持传入参数 doqueryint; 支持传入参数 doqueryone; 支持传入参数 doqueryobject; 支持传入参数 doqueryobjectarray 支持传入参数 doquerylist; 支持传入参数 getCLOB；
    构造 PreparedStatement createStatement ptmt 支持对象传入动态sql值
    
    git-svn-id: http://buildserver:9880/framework@283 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/db/oracle/DatabaseManager.java
+++ trunk/java/src/nds/db/oracle/DatabaseManager.java
@@ -4,21 +4,33 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.math.BigDecimal;
+
+import java.sql.Clob;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 import java.util.Iterator;
 import java.util.Properties;
 import oracle.sql.CLOB;
 import java.io.Writer;
-
+import java.util.List;
 import nds.db.DBManager;
 import nds.log.Logger;
 import nds.log.LoggerManager;
-import nds.query.QueryEngine;
+import org.json.*;
 import nds.query.*;
-import nds.query.SPResult;
 import nds.util.Tools;
 import nds.schema.*;
+import oracle.jdbc.OracleConnection;
 import org.jboss.resource.adapter.jdbc.WrappedConnection;
 
 public class DatabaseManager implements DBManager{
@@ -54,40 +66,595 @@ public class DatabaseManager implements DBManager{
     /*
      * This method uses temporary clob to create the CLOB object.
      */
-    public static CLOB getCLOB( StringBuffer clobData,oracle.jdbc.OracleConnection conn )
-                    throws SQLException {
-      CLOB tempClob = null;
+	public static CLOB getCLOB(StringBuffer clobData,
+			oracle.jdbc.OracleConnection conn) throws SQLException {
+		CLOB tempClob = null;
 
-      try {
-        //  create a new temporary CLOB
-        tempClob = CLOB.createTemporary( conn, true, CLOB.DURATION_SESSION );
+		try {
+			// create a new temporary CLOB
+			tempClob = CLOB.createTemporary(conn, true, CLOB.DURATION_SESSION);
 
-        // Open the temporary CLOB in readwrite mode to enable writing
-        tempClob.open( CLOB.MODE_READWRITE );
+			// Open the temporary CLOB in readwrite mode to enable writing
+			tempClob.open(CLOB.MODE_READWRITE);
 
-        // Get the output stream to write
+			// Get the output stream to write
 
-        Writer tempClobWriter = tempClob.setCharacterStream( 0L );
+			Writer tempClobWriter = tempClob.setCharacterStream(0L);
 
-        // Write the data into the temporary CLOB
-        tempClobWriter.write( clobData.toString() );
+			// Write the data into the temporary CLOB
+			tempClobWriter.write(clobData.toString());
 
-        // Flush and close the stream
-        tempClobWriter.flush(  );
-        tempClobWriter.close(  );
+			// Flush and close the stream
+			tempClobWriter.flush();
+			tempClobWriter.close();
 
-        // Close the temporary CLOB
-        tempClob.close(  );
+			// Close the temporary CLOB
+			tempClob.close();
 
-      } catch ( Exception exp ) {
-    	  logger.error("fail to getClob",exp);
-	         // Free CLOB object
-          tempClob.freeTemporary( );
-          // do something
-      }
-      return tempClob;      
+		} catch (Exception exp) {
+			logger.error("fail to getClob", exp);
+			// Free CLOB object
+			tempClob.freeTemporary();
+			// do something
+		}
+		return tempClob;
 
-    }
+	}
+
+	public static CLOB getCLOB(String clobstr,
+			oracle.jdbc.OracleConnection conn) throws SQLException {
+		CLOB tempClob = null;
+		try {
+			// create a new temporary CLOB
+			tempClob = CLOB.createTemporary(conn, true, CLOB.DURATION_SESSION);
+
+			// Open the temporary CLOB in readwrite mode to enable writing
+			tempClob.open(CLOB.MODE_READWRITE);
+
+			Writer tempClobWriter = tempClob.setCharacterStream(0L);
+			tempClobWriter.write(clobstr);
+			// Flush and close the stream
+			tempClobWriter.flush();
+			tempClobWriter.close();
+
+			tempClob.close();
+		} catch (Exception exp) {
+			logger.error("fail to getClob", exp);
+
+			tempClob.freeTemporary();
+		}
+
+		return tempClob;
+	}
+	
+	public static CLOB getCLOB(StringBuilder clobstr,
+			oracle.jdbc.OracleConnection conn) throws SQLException {
+		CLOB tempClob = null;
+		try {
+			// create a new temporary CLOB
+			tempClob = CLOB.createTemporary(conn, true, CLOB.DURATION_SESSION);
+
+			// Open the temporary CLOB in readwrite mode to enable writing
+			tempClob.open(CLOB.MODE_READWRITE);
+
+			Writer tempClobWriter = tempClob.setCharacterStream(0L);
+			tempClobWriter.write(clobstr.toString());
+			// Flush and close the stream
+			tempClobWriter.flush();
+			tempClobWriter.close();
+
+			tempClob.close();
+		} catch (Exception exp) {
+			logger.error("fail to getClob", exp);
+
+			tempClob.freeTemporary();
+		}
+
+		return tempClob;
+	}
+	/**
+	 * 
+	 * @param sql
+	 * @param paramArrayOfObject
+	 * @param conn
+	 * @param toUpper
+	 * @return
+	 * @throws QueryException
+	 */
+	public JSONArray doQueryObjectArray(String sql,Object[] paramArrayOfObject, Connection conn,
+			boolean toUpper) throws QueryException {
+		JSONArray jor_Array  = new JSONArray();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			int m = 0;
+			long startTime = System.currentTimeMillis();
+			pstmt = createStatement(sql, paramArrayOfObject, conn);
+			ResultSetMetaData rs_metadata=rs.getMetaData();
+			rs = pstmt.executeQuery();
+			int colCount = rs.getMetaData().getColumnCount();
+			
+			while (rs.next()) {
+				JSONObject jor = new JSONObject();
+				for (int k = 1; k <= colCount; k++) {
+					Object localObject = rs.getObject(k);
+					String str = toUpper ? rs_metadata
+							.getColumnName(k).toUpperCase()
+							: rs_metadata.getColumnName(k)
+									.toLowerCase();
+					if (rs.wasNull()) {
+						jor.put(str, JSONObject.NULL);
+					} else {
+						if ((localObject instanceof Clob)) {
+							localObject = ((Clob) localObject).getSubString(1L,
+									(int) ((Clob) localObject).length());
+						}
+						jor.put(str, localObject);
+					}
+				}
+
+				jor_Array.put(jor);
+				m++;
+			}
+			double duration = (int) ((System.currentTimeMillis() - startTime) / 1000.0);
+ 
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int v=1; v < paramArrayOfObject.length; v++)
+					param.append(",").append(paramArrayOfObject[v]);
+			}
+			param.append("]");
+
+			if (duration > logDuration)
+				logger.info("(" + duration + " s, cnt " + m+ ") " + sql + param);
+			else
+				logger.debug("(" + duration + " s, cnt " + m+ ") " + sql + param);
+
+			return jor_Array;
+			
+		} catch (Exception e) {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int i = 1; i < paramArrayOfObject.length; i++)
+					param.append(",")
+							.append(paramArrayOfObject[i]);
+			}
+			param.append("]");
+
+			logger.error("Error doing query:"+ sql + param + ":" + e);
+			throw new QueryException("Error doing query:" + sql,e);
+		} finally {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param sql
+	 * @param paramArrayOfObject
+	 * @param conn
+	 * @param toUpper
+	 * @return
+	 * @throws QueryException
+	 */
+	public JSONObject doQueryObject(String sql,
+			Object[] paramArrayOfObject, Connection conn,
+			boolean toUpper) throws QueryException {
+		JSONObject jor = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			int m = 0;
+			long startTime = System.currentTimeMillis();
+			pstmt = createStatement(sql, paramArrayOfObject, conn);
+			ResultSetMetaData rs_metadata=rs.getMetaData();
+			rs = pstmt.executeQuery();
+			int colCount = rs.getMetaData().getColumnCount();
+
+			while (rs.next()) {
+				jor = new JSONObject();
+				for (int k = 1; k <= colCount; k++) {
+					Object localObject = rs.getObject(k);
+					String str = toUpper ? rs_metadata
+							.getColumnName(k).toUpperCase()
+							: rs_metadata.getColumnName(k)
+									.toLowerCase();
+					if (rs.wasNull()) {
+						jor.put(str, JSONObject.NULL);
+					} else {
+						if ((localObject instanceof Clob)) {
+							localObject = ((Clob) localObject).getSubString(1L,
+									(int) ((Clob) localObject).length());
+						}
+						jor.put(str, localObject);
+					}
+				}
+				m++;
+			}
+			
+
+			double duration = (int) ((System.currentTimeMillis() - startTime) / 1000.0);
+			 
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int v=1; v < paramArrayOfObject.length; v++)
+					param.append(",").append(paramArrayOfObject[v]);
+			}
+			param.append("]");
+
+			if (duration > logDuration)
+				logger.info("(" + duration + " s, cnt " + m+ ") " + sql + param);
+			else
+				logger.debug("(" + duration + " s, cnt " + m+ ") " + sql + param);
+
+			return jor;
+			
+		} catch (Exception e) {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int i = 1; i < paramArrayOfObject.length; i++)
+					param.append(",")
+							.append(paramArrayOfObject[i]);
+			}
+			param.append("]");
+
+			logger.error("Error doing query:"+ sql + param + ":" + e);
+			throw new QueryException("Error doing query:" + sql,e);
+		} finally {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+		}
+	}
+/**
+ * 
+ * @param sql
+ * @param paramArrayOfObject
+ * @param paramInt
+ * @param conn
+ * @return
+ * @throws QueryException
+ */
+	public JSONArray doQueryJSONArray(String sql, Object[] paramArrayOfObject,
+			int paramInt, Connection conn) throws QueryException {
+		JSONArray jor_arry = new JSONArray();
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+
+			long startTime = System.currentTimeMillis();
+			//Connection pcon=getConnection();
+			pstmt = createStatement(sql, paramArrayOfObject, conn);
+			//rs = pstmt.executeQuery();
+			//int p=1741;
+			//pstmt= conn.prepareStatement(sql);
+			//pstmt.setInt(1, p);
+			rs = pstmt.executeQuery();
+			ResultSetMetaData rs_metadata = rs.getMetaData();
+			int colCount = rs.getMetaData().getColumnCount();
+			int i = 0;
+			Object local;
+			logger.debug("colCount is "+colCount);
+			logger.debug("paramInt is "+paramInt);
+			if (colCount == 1) {
+				while (rs.next()) {
+					if(paramInt == i)break;
+					local = rs.getObject(1);
+					if (rs.wasNull())
+						jor_arry.put(JSONObject.NULL);
+					else {
+						jor_arry.put(local);
+					}
+					i++;
+				}
+			} else {
+				logger.debug("do while");
+				while (rs.next()) {
+					//logger.debug("get rs");
+					if(paramInt == i)break;
+					JSONArray rs_jor = new JSONArray();
+					for (int m = 1; m <= colCount; m++) {
+						Object localObject = rs.getObject(m);
+						//logger.debug("get g");
+						//logger.debug(String.valueOf(rs.getObject(m)));
+						if (rs.wasNull()){
+							//logger.debug("get null");
+							rs_jor.put(JSONObject.NULL);
+						}else{
+							//logger.debug("get object");
+							rs_jor.put(rs.getObject(m));
+						}
+					}
+					jor_arry.put(rs_jor);
+					i++;
+				}
+			}
+			logger.debug("jor_arry "+jor_arry.toString());
+			
+			double duration = (int) ((System.currentTimeMillis() - startTime) / 1000.0);
+
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int v = 1; v < paramArrayOfObject.length; v++)
+					param.append(",").append(paramArrayOfObject[v]);
+			}
+			param.append("]");
+
+			if (duration > logDuration)
+				logger.info("(" + duration + " s, cnt " + i + ") " + sql
+						+ param);
+			else
+				logger.debug("(" + duration + " s, cnt " + i + ") " + sql
+						+ param);
+
+			return jor_arry;
+
+		} catch (Exception e) {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+			StringBuilder param = new StringBuilder("[");
+			if ((paramArrayOfObject != null) && (paramArrayOfObject.length > 0)) {
+				param.append(paramArrayOfObject[0]);
+				for (int i = 1; i < paramArrayOfObject.length; i++)
+					param.append(",").append(paramArrayOfObject[i]);
+			}
+			param.append("]");
+
+			logger.error("Error doing query:" + sql + param + ":" + e);
+			throw new QueryException("Error doing query:" + sql, e);
+		} finally {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception ew) {
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param sql
+	 * @param paramArrayOfObject
+	 * @param conn
+	 * @return
+	 * @throws SQLException
+	 * @throws QueryException
+	 */
+	private static PreparedStatement createStatement(String sql,
+			Object[] paramArrayOfObject, Connection conn) throws SQLException,
+			QueryException {
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		if (paramArrayOfObject != null)
+			logger.debug("createStatement now!");
+			for (int i = 0; i < paramArrayOfObject.length; i++) {
+				Object parm = paramArrayOfObject[i];
+				logger.debug("localObject is "+String.valueOf(parm));
+				if (paramArrayOfObject == null) {
+					throw new QueryException(
+							"Intenal Error: unsupport null as parameter");
+				}
+				if ((parm instanceof String)) {
+					logger.debug("createStatement String!");
+					pstmt.setString(i + 1, (String) parm);
+				} else if ((parm instanceof Integer)) {
+					int val=((Integer) parm).intValue();
+					logger.debug("createStatement Integer! val is"+String.valueOf(val));
+					pstmt.setInt(i + 1, val);
+				} else if ((parm instanceof Float)) {
+					logger.debug("createStatement Float!");
+					pstmt.setFloat(i + 1, ((Float) parm).floatValue());
+				} else if ((parm instanceof Double)) {
+					logger.debug("createStatement Double!");
+					pstmt.setDouble(i + 1, ((Double) parm).doubleValue());
+				} else if ((parm instanceof Date)) {
+					logger.debug("createStatement Date!");
+					pstmt.setTimestamp(i + 1, new Timestamp(
+							((Date) parm).getTime()));
+				} else if (((parm instanceof StringBuilder))
+						|| ((parm instanceof StringBuffer))) {
+					logger.debug("createStatement StringBuffer!");
+					// converting conn to oracle.jdbc.OracleConnection
+					oracle.jdbc.OracleConnection oc = null;
+					if ((conn instanceof WrappedConnection)) {
+						oc = (OracleConnection) ((WrappedConnection) conn)
+								.getUnderlyingConnection();
+					} else if ((conn instanceof OracleConnection))
+						oc = (OracleConnection) conn;
+					else {
+						throw new SQLException(
+								"Not supported connection class:"
+										+ oc.getClass().getName()
+										+ " (only oracle and jboss connection supported)");
+					}
+					pstmt.setClob(i + 1, getCLOB(parm.toString(), oc));
+				} else if ((parm instanceof Clob)) {
+					logger.debug("createStatement Clob!");
+					pstmt.setClob(i + 1, (Clob) parm);
+				} else if ((parm instanceof Class)) {
+					logger.debug("instanceof Class!");
+					if (parm.equals(String.class))
+						pstmt.setNull(i + 1, 12);
+					else if (parm.equals(Integer.class))
+						pstmt.setNull(i + 1, 4);
+					else if (parm.equals(Float.class))
+						pstmt.setNull(i + 1, 6);
+					else if (parm.equals(Double.class))
+						pstmt.setNull(i + 1, 8);
+					else if (parm.equals(Date.class))
+						pstmt.setNull(i + 1, 93);
+					else if (parm.equals(StringBuilder.class))
+						pstmt.setNull(i + 1, 2005);
+					else if (parm.equals(StringBuffer.class))
+						pstmt.setNull(i + 1, 2005);
+					else if (parm.equals(Clob.class))
+						pstmt.setNull(i + 1, 2005);
+					else
+						throw new QueryException(
+								"Intenal Error: unsupported type:"
+										+ parm);
+				} else {
+					throw new QueryException("Intenal Error: unsupported type:"
+							+ parm.getClass() + ",value=" + parm);
+				}
+			}
+		return pstmt;
+	}
+
+ /**
+  * 
+  */
+	public List doQueryList(String sql, Object[] paramArrayOfObject,
+			int paramInt, Connection conn) throws QueryException {
+		ArrayList al = new ArrayList();
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			long startTime = System.currentTimeMillis();
+			pstmt = createStatement(sql, paramArrayOfObject, conn);
+			rs = pstmt.executeQuery();
+
+			double duration = (int) ((System.currentTimeMillis() - startTime) / 1000.0);
+	
+			int colCount = rs.getMetaData().getColumnCount();
+			int i=0;
+			if (colCount == 1) {
+				 
+				while (rs.next()) {
+					if(paramInt == i)break;
+					al.add(rs.getObject(1));
+					i++;
+				} 
+ 			
+				  
+			} else {
+				while (rs.next()) {
+					//logger.debug("get rs");
+					if(paramInt == i)break;
+					List rec = new java.util.ArrayList();
+					for (int m = 0; m <= colCount;m++) {
+						rec.add(rs.getObject(m));
+					}
+					al.add(rec);
+					i++;
+				    
+				}
+			}
+			if (duration > logDuration)
+				logger.info("(" + duration + " s, cnt " + i + ") " + sql);
+			else
+				logger.debug("(" + duration + " s, cnt " + i + ") " + sql);
+			
+			return al;
+		} catch (SQLException e) {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception e2) {
+			}
+			logger.error("Error doing query::" + sql + "::" + e);
+			throw new QueryException("Error doing query:" + sql, e);
+		} finally {
+			try {
+				pstmt.close();
+			} catch (Exception ea) {
+			}
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+
+		}
+	}
+    /**
+     * 
+     * @param sql
+     * @param paramArrayOfObject
+     * @param conn
+     * @return
+     * @throws QueryException
+     */
+    
+	public int executeUpdate(String sql, Object[] paramArrayOfObject,
+			Connection conn) throws QueryException {
+		PreparedStatement pstmt = null;
+		try {
+			long startTime = System.currentTimeMillis();
+			pstmt = createStatement(sql, paramArrayOfObject, conn);
+			
+			double duration = (int) ((System.currentTimeMillis() - startTime) / 1000.0);
+			if (duration > logDuration)
+				logger.info("(" + duration + " s) " + sql);
+			else
+				logger.debug("(" + duration + " s) " + sql);
+			return  pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			try {
+				pstmt.close();
+			} catch (Exception ew) {
+			}
+			try {
+				conn.close();
+			} catch (Exception ea) {
+			}
+			StringBuilder param = new StringBuilder();
+			for (int i = 0; i < paramArrayOfObject.length - 1; i++)
+				param.append(paramArrayOfObject[i]).append(", ");
+			param.append(paramArrayOfObject[(paramArrayOfObject.length - 1)]);
+			logger.error("Error doing update:"+ sql + " (" + param + "):"+ e);
+			throw new QueryException("Error doing query:" + sql,e);
+		} finally {
+			try {
+				pstmt.close();
+			} catch (Exception ew) {
+			}
+		}
+	}
 
     /**
      * @param params should contain:
