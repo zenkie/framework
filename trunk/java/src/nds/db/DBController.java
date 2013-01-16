62b3acac408e782e6f49e0cb17c676d967e00c02
jackrain <jackrain@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-01-16 14:14:37 +0000

    增加 DBController.java 接口方法 executeUpdate; doqueryint; 支持传入参数 doqueryone; 支持传入参数 doqueryobject; 支持传入参数 doqueryobjectarray 支持传入参数 doquerylist; 支持传入参数
    git-svn-id: http://buildserver:9880/framework@281 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/db/DBController.java
+++ trunk/java/src/nds/db/DBController.java
@@ -3,14 +3,14 @@ package nds.db;
 import java.sql.Connection;
 import java.util.Collection;
 import java.util.Properties;
-
+import java.util.List;
 import nds.log.Logger;
 import nds.log.LoggerManager;
 import nds.query.QueryException;
 import nds.query.QueryRequestImpl;
 import nds.query.*;
 import nds.util.Loader;
-
+import org.json.*;
 /**
  * Work as proxy for respective database managers, the dbtype should be defined
  * as "dbms.type" in configuration file, and there should exists Class
@@ -85,6 +85,44 @@ public class DBController implements java.io.Serializable, DBManager,
         return manager.getSheetStatus(tableName, tableId);
     }
 
+	public List doQueryList(String sql, Object[] paramArrayOfObject,
+			int paramInt, Connection conn) throws QueryException {
+		return manager.doQueryList(sql, paramArrayOfObject, paramInt, conn);
+	}
+	
+	public JSONArray doQueryJSONArray(String sql,
+			Object[] paramArrayOfObject, int paramInt,
+			Connection conn) throws QueryException {
+		return manager.doQueryJSONArray(sql, paramArrayOfObject,
+				paramInt, conn);
+	}
+
+	public JSONArray doQueryObjectArray(String sql,
+			Object[] paramArrayOfObject, Connection conn,
+			boolean toUpper) throws QueryException {
+		return manager.doQueryObjectArray(sql, paramArrayOfObject,
+				conn, toUpper);
+	}
+
+	public int executeUpdate(String sql, Object[] paramArrayOfObject,
+			Connection conn) throws QueryException {
+		return manager.executeUpdate(sql, paramArrayOfObject,
+				conn);
+	}
+
+	public JSONObject doQueryObject(String sql,
+			Object[] paramArrayOfObject, Connection conn)
+			throws QueryException {
+		return manager.doQueryObject(sql, paramArrayOfObject,
+				conn, true);
+	}
+
+	public JSONObject doQueryObject(String sql,
+			Object[] paramArrayOfObject, Connection conn,
+			boolean toUpper) throws QueryException {
+		return manager.doQueryObject(sql, paramArrayOfObject,
+				conn, toUpper);
+	}
 
 
 }
