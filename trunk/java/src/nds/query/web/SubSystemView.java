1ef4078f0cdc37a379dcd73aa8405ea8702e5ab9
jackrain <jackrain@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-02-04 08:16:32 +0000

    git-svn-id: http://buildserver:9880/framework@306 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/query/web/SubSystemView.java
+++ trunk/java/src/nds/query/web/SubSystemView.java
@@ -135,7 +135,8 @@ public class SubSystemView {
 					sub_list = subsystems.split(",");
 					for (int m = 0; m < sub_list.length; m++)
 					{
-						SubSystem usersub = manager.getSubSystem((String)sub_list[m].trim());
+						String subname= (String)sub_list[m].trim();
+						SubSystem usersub = manager.getSubSystem(subname);
 						if (usersub != null)
 						{
 							
