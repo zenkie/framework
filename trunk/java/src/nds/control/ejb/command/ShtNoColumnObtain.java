2d586564bf9dbbdabf73d3183d0d996740a4c9fa
jackrain <jackrain@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-02-28 02:38:11 +0000

    //系统识别当前格式是SQL的依据是：当前格式中含有“$nextval”字符串。否则都将作为VTL 格式来处理。 当为VTL格式时 采用velocity模版解析模式支持
    git-svn-id: http://buildserver:9880/framework@312 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/nds/control/ejb/command/ShtNoColumnObtain.java
+++ trunk/java/src/nds/control/ejb/command/ShtNoColumnObtain.java
@@ -1,12 +1,18 @@
 package nds.control.ejb.command;
+import java.io.StringWriter;
 import java.rmi.RemoteException;
 import java.util.Vector;
 
 import nds.control.event.DefaultWebEvent;
+import nds.control.util.ShtNoVariables;
 import nds.query.QueryEngine;
 import nds.schema.Column;
 import nds.schema.Table;
 import nds.util.*;
+import nds.velocity.VelocityUtils;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.context.Context;
 /**
  * Title:        NDS Project
  * Description:  San gao shui yuan, mu xiang ren jia
@@ -38,10 +44,16 @@ public class ShtNoColumnObtain extends ColumnObtain{
       logger.debug("clientId="+ clientId);
 
       String sheetNo = null;
+      
+     
+      sheetNo=(String)engine.doQueryOne("select vformat from ad_sequence where ad_client_id=? and name=?", new Object[] { Integer.valueOf(clientId), col.getSequenceHead() }, getConnection());
+   
+      //系统识别当前格式是SQL的依据是：当前格式中含有“$nextval”字符串。否则都将作为VTL 格式来处理。
+      int VTL=(sheetNo==null)||(sheetNo.contains("$nextval"))? 1 : 0;
+      logger.debug("sheetNo="+sheetNo+" VTL="+ VTL);
 
       String[] value = event.getParameterValues(col.getName());
-      
-      
+
       String[] sheetNoSeq = new String[length];
 
       for(int i = 0;i<length;i++){
@@ -54,22 +66,37 @@ public class ShtNoColumnObtain extends ColumnObtain{
             	if(value!=null && value.length>i && Validator.isNotNull(value[i])){
             		sheetNoSeq[i] = value[i];
             	}else{
-		              String resultCol =engine.getSheetNo(col.getSequenceHead(), clientId);
-		              sheetNoSeq[i] = resultCol;
+            		if(VTL==1){
+            			String resultCol =engine.getSheetNo(col.getSequenceHead(), clientId);
+            			sheetNoSeq[i] = resultCol;
+            		}else{
+                		StringWriter localStringWriter = new StringWriter(); 
+                		ShtNoVariables dd = new ShtNoVariables(col, event ,i,getConnection()); 
+                		VelocityContext context = VelocityUtils.createContext();
+                		context.put("obj", dd);
+                		//vlelocity 模版解析输出
+                		Velocity.evaluate(context, localStringWriter, ShtNoVariables.class.getName(), sheetNo);
+                		sheetNo  = localStringWriter.getBuffer().toString(); 
+                		logger.debug("evaluate sheetNo="+sheetNo);
+                		sheetNo  = dd.replaceFlowNO(sheetNo );
+            			sheetNoSeq[i] = sheetNo ;
+            			}
+		         
             	}
-              }catch(NDSException e){
-                  if( this.isBestEffort ){
-                      this.setRowInvalid(i, "在获取编号时出错："+ e);
-                      sheetNoSeq[i] = "Invalid";
-                  }else throw e;
-              }
+				} catch (Throwable e) {
+					if (this.isBestEffort) {
+						this.setRowInvalid(i, "在获取编号时出错：" + e);
+						sheetNoSeq[i] = "Invalid";
+					} else {
+						if (e instanceof NDSException)
+							throw ((NDSException) e);
+						throw new NDSException(e.getMessage(), e);
+					}
+				}
           }
       }
       vec.add(sheetNoSeq) ;
       return vec;
   }
-/*  public int getLength(){
-      return length;
-  }
-  */
+
 }
