93402798f68b453e0cf2888d3edf64c2c20e1442
jackdev <jackdev@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-05-17 03:16:34 +0000

    增加支持多物理地址验证支持 分号分隔开
    git-svn-id: http://buildserver:9880/framework@403 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

---
+++ trunk/java/src/com/getMAC/multiMac.java
@@ -0,0 +1,31 @@
+package com.getMAC;
+
+
+public class multiMac {
+	/**
+	 * 分号
+	 */
+	private static final String DISPATCH = ";";
+	/**
+	 * 对多台机器的Mac地址进行判断
+	 * @param multiMac
+	 * @return 是否为特定的Mac地址
+	 */
+	public static int multiMacJun(String multiMac){
+		int returnValue = 0;
+		// 如果当前为一台机器的Mac地址
+		if(multiMac.indexOf(DISPATCH) == -1){
+			  //  执行单一的测试 
+			returnValue = checkMACAddr.checkMAC(multiMac);
+		} else {
+			 String[] macStrArr = new String[]{};
+			 macStrArr = multiMac.split(DISPATCH);
+			 for(int i=0;i<macStrArr.length;i++){
+				 String macArr = macStrArr[i];
+				 returnValue = checkMACAddr.checkMAC(macArr);
+				 if(returnValue == 1) break;
+			 }
+		}
+		return returnValue;
+	}
+}
