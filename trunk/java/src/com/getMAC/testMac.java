3050abd0c5e4f6179478366c210cf6b4c599b55c
jackdev <jackdev@1e16ec6f-bb07-4e9e-848b-f64f0d098a42>
2013-05-21 08:01:48 +0000

    支持 增加支持多物理地址验证支持 分号分隔开 测试
    git-svn-id: http://buildserver:9880/framework@404 1e16ec6f-bb07-4e9e-848b-f64f0d098a42

--- trunk/java/src/com/getMAC/testMac.java
+++ trunk/java/src/com/getMAC/testMac.java
@@ -9,11 +9,14 @@ public class testMac {
 	  
 	AES aes=new AES("burgeon");
 	//String str = Tools.encrypt(GetMACH.getMach());
-	String str = GetMACH.get_maconly();//aes.encrypt(GetMACH.getMach());
+	String str = GetMACH.get_maconly();
+	String str1 = aes.encrypt("4,6A:1E:20:52:41:53,58:94:23:54:8E:8E,00:03:05:17:03:25,68:94:23:54:8E:8D,2A:94:23:54:8E:8D;");
+	String str2 = aes.encrypt("3,9A:1E:20:52:41:53,68:94:23:54:8E:8E,00:03:05:17:03:25,68:94:23:54:8E:8D,2A:94:23:54:8E:8D;");
+	//System.out.print(aes.decrypt(str1+str+str2));
 	//System.out.print(GetMACH.getMach());
 	//运行结果
-	System.out.print(str);
-	int result = checkMACAddr.checkMAC(aes.decrypt(str));
+	//System.out.print(str);
+	int result = multiMac.multiMacJun(aes.decrypt(str+str1+str2));
 	if(result == 0){
 		System.out.println("没有权限");
 	} else if(result == 1){
