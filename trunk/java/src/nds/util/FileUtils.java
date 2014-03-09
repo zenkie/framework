3d56f5222f6581c9b63ee40d83f04cbfa9020244
jackrain <jackrsky@gmail.com>
2014-03-09 06:57:17 -0700

    Java文件操作 获取不带扩展名的文件名

--- trunk/java/src/nds/util/FileUtils.java
+++ trunk/java/src/nds/util/FileUtils.java
@@ -181,6 +181,23 @@ public class FileUtils {
     	if(p > 0 ) return fileName.substring(p+1);
     	return null;
     }
+    
+	/**
+	 * Java文件操作 获取不带扩展名的文件名
+	 * 
+	 * Created on: 2011-8-2 Author: blueeagle
+	 */
+	public static String getFileNameNoEx(String filename) {
+		if ((filename != null) && (filename.length() > 0)) {
+			int dot = filename.lastIndexOf('.');
+			if ((dot > -1) && (dot < (filename.length()))) {
+				return filename.substring(0, dot);
+			}
+		}
+		return filename;
+	}
+        
+    
     /**
      * Copy a file
      * @param src source file
