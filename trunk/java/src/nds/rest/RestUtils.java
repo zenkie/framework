216e6723941847d6264c68aa6a29f1084208fddb
jackrain <jackrsky@gmail.com>
2014-03-28 11:02:13 +0800

    增加 POST 请求方法的支持

--- trunk/java/src/nds/rest/RestUtils.java
+++ trunk/java/src/nds/rest/RestUtils.java
@@ -15,7 +15,53 @@ public class RestUtils {
 	public static ValueHolder sendRequest(String apiURL, Map<String, String> params, String method) throws Exception {
 
         String queryString = (null == params) ? "" : delimit(params.entrySet(),true	);
+		logger.debug("queryString :"+queryString);
+        HttpURLConnection conn = (HttpURLConnection) new URL(apiURL).openConnection();
+        conn.setConnectTimeout(6* 1000);
+		conn.setRequestMethod(method);
+		conn.setDoOutput(true);
+		conn.connect();
+		conn.getOutputStream().write(queryString.getBytes());
+		String charset = getChareset(conn.getContentType());
+		if (conn.getResponseCode() != 200) throw new NDSException("«Î«Ûurl ß∞‹");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
+		StringBuffer buffer = new StringBuffer();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			buffer.append(line);
+		}
+		logger.debug(Tools.toString( conn.getHeaderFields()));
+        SipStatus status =SipStatus.getStatus( conn.getHeaderField("sip_status"));
+		reader.close();
+        conn.disconnect();
+		String msg= buffer.toString();
+		ValueHolder vh=new ValueHolder();
+
+		int code=0;
+		if(status!=null){
+			if(status.equals( SipStatus.SUCCESS)){
+				code=0;
+			}else{
+				code=- nds.util.Tools.getInt(status.getCode(),1);
+				if(code==0){
+					// SipStatus.ERROR =0
+					code=-1;
+				}
+			}
+		}
 		
+		vh.put("code",String.valueOf(code));
+		vh.put("message", msg);
+		vh.put("queryString",queryString );
+        //SipResult result = new SipResult(SipStatus.getStatus(code),buffer.toString());
+
+        return vh;
+	}
+	
+	public static ValueHolder sendRequest_buff(String apiURL, String params, String method) throws Exception {
+
+        String queryString = (null == params) ? "" : params;
+        logger.debug("queryString :"+queryString);
         HttpURLConnection conn = (HttpURLConnection) new URL(apiURL).openConnection();
 		conn.setRequestMethod(method);
 		conn.setDoOutput(true);
@@ -76,7 +122,8 @@ public class RestUtils {
 				notFirst = true;
 			}
 			Object value = entry.getValue();
-			buffer.append(entry.getKey()).append("=").append(
+			if(value==null)value="";
+			buffer.append(doEncode?URLEncoder.encode(entry.getKey(), "UTF8"):entry.getKey()).append("=").append(
 					doEncode ? URLEncoder.encode(value.toString(), "UTF8") : value);
 		}
 		return buffer.toString();
