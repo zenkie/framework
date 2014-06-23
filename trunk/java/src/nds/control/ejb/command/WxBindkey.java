ef1325aa01869f605b43e711ffb5ff20cc8b91f1
jackrain <jackrsky@gmail.com>
2014-06-23 11:16:33 +0800

    增加公众号智能绑定功能！wxbindkey

---
+++ trunk/java/src/nds/control/ejb/command/WxBindkey.java
@@ -0,0 +1,127 @@
+package nds.control.ejb.command;
+
+import java.io.File;
+import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Vector;
+
+import org.json.JSONObject;
+
+import com.kin.weixin.WeixinBind;
+
+import nds.control.ejb.Command;
+import nds.control.event.DefaultWebEvent;
+import nds.control.event.NDSEventException;
+import nds.control.util.ValueHolder;
+import nds.control.web.UserWebImpl;
+import nds.control.web.WebUtils;
+import nds.control.web.binhandler.Wxvcode;
+import nds.query.QueryEngine;
+import nds.schema.Column;
+import nds.schema.Table;
+import nds.schema.TableManager;
+import nds.security.User;
+import nds.util.Configurations;
+import nds.util.NDSException;
+import nds.util.Tools;
+import nds.util.WebKeys;
+import nds.weixin.ext.WeUtils;
+import nds.weixin.ext.WeUtilsManager;
+
+public class WxBindkey extends Command {
+
+	public static final String PROP_STORAGEDIR = "webclient.upload";
+	private String m_storageDir;
+	
+	@Override
+	public ValueHolder execute(DefaultWebEvent event) throws NDSException,
+			RemoteException {
+		ValueHolder vh= new ValueHolder();
+		JSONObject rs=new JSONObject();
+	  	JSONObject jo=(JSONObject)event.getParameterValue("jsonObject");
+	  	JSONObject params=null;
+  	    QueryEngine engine=QueryEngine.getInstance();
+        Connection conn= engine.getConnection();
+        PreparedStatement pstmt=null;
+    	try{
+   		 Configurations conf=(Configurations)WebUtils.getServletContextManager().getActor(WebKeys.CONFIGURATIONS);
+	     m_storageDir = conf.getProperty(PROP_STORAGEDIR, "/act.net/webhome");
+	     User usr=helper.getOperator(event);
+	     int clientId= usr.adClientId; 
+	     	params=jo.getJSONObject("params");
+	     	String login_user=params.getString("user");
+	   		String login_pwd=params.getString("pwd");
+	   		String vcode=params.optString("vcode","");
+	   		String userid=params.getString("userid");
+    		String url = null,token = null;
+   
+    		//vcode
+    		Wxvcode wxv=Wxvcode.getInstance();
+    		WeixinBind wx = new WeixinBind(login_user, login_pwd,wxv.getcookei(userid));
+    		wx.login(vcode);
+    		if(wx.getLoginErrCode()==0){
+    		
+    		if(!wx.isDevUser(wx.getToken())){
+    			vh.put("message", "尚未成为开发者，请在公众平台中检查相应的注册信息！");
+    			vh.put("code","-1");
+    			return vh;
+    		}
+    		
+    		wx.editDevInteface(wx.getToken());//编辑开发模式
+    		
+    		
+    		List li=engine.doQueryList("select url,wxtoken from WX_INTERFACESET t where t.ad_client_id="+clientId,conn);
+	        if (li.size() > 0) {
+	        	url = String.valueOf(((List)li.get(0)).get(0));
+	        	token = String.valueOf(((List)li.get(0)).get(1));
+	         }
+    		wx.editCommonInteface(wx.getToken(),url,token);
+    		WeUtilsManager Wemanage =WeUtilsManager.getInstance();
+			WeUtils wu=Wemanage.getByAdClientId(usr.adClientId);
+    		String svrPath = m_storageDir+ File.separator  + wu.getDoMain();
+    		
+    		wx.editServiceOAuth(wx.getToken(), wu.getDoMain());//oauth2.0
+    		
+    		File uploadPath = new File(svrPath);//上传文件目录
+    		logger.debug("uploadPath ->"+svrPath);
+    	    if (!uploadPath.exists()) {
+    	       uploadPath.mkdirs();
+    	    }
+    		svrPath+=File.separator+"wxappcode.jpg";
+    		wx.getQrcode(wx.getToken(),svrPath);
+    		
+    		//QueryEngine.getInstance().executeUpdate(sql)
+    		pstmt= conn.prepareStatement("update WEB_CLIENT set qrcode=? where ad_client_id=?");		
+   			pstmt.setString(1,"/servlets/userfolder/wxappcode.jpg");
+   			pstmt.setInt(2,clientId);
+		    pstmt.executeUpdate();
+		    //set OriginalID
+    		pstmt= conn.prepareStatement("update WX_INTERFACESET set originalid=? where ad_client_id=?");		
+   			pstmt.setString(1,wx.getOriginalID(wx.getToken()));
+   			pstmt.setInt(2,clientId);
+		    pstmt.executeUpdate();
+		    
+    		vh.put("message", wx.getLoginErrMsg());
+			vh.put("code","0");
+    		}else if(wx.getLoginErrCode()==-8||wx.getLoginErrCode()==-27){
+    			vh.put("message", wx.getLoginErrMsg());
+    			vh.put("code","-8");
+    		}else{
+    			System.out.print("getLoginErrCode->"+wx.getLoginErrCode());
+    			vh.put("message", wx.getLoginErrMsg());
+    			vh.put("code","-1");
+    		}
+    		
+   		}catch(Throwable t){
+   	  		if(t instanceof NDSException) throw (NDSException)t;
+   	  		logger.error("exception",t);
+   	  		throw new NDSException(t.getMessage(), t);	
+   	 	}finally{
+   	        try{pstmt.close();}catch(Exception ea){}
+   	        try{conn.close();}catch(Exception e){}
+   	  	} 
+    	return vh;
+	}
+}
