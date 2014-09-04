043edb886a4cbc781c3120526964fddde4fb767b
jackrain <jackrsky@gmail.com>
2014-09-04 11:29:28 +0800

    增加 主办 硬盘的 uid 识别

--- trunk/java/src/com/getMAC/NetInterfaceData.java
+++ trunk/java/src/com/getMAC/NetInterfaceData.java
@@ -147,6 +147,87 @@ import java.io.InputStreamReader;
             }  
             return result.trim().replace("\"","");//.replace("-", "");  
         }  
+        
+        /**
+        * 获取硬盘序列号
+        * 
+        * @param drive
+        *            盘符
+        * @return
+        */
+        public static String getHardDiskSN(String drive) {
+        String result = "";
+        String os = getOsName();  
+        if (os.startsWith("Mac")) return "";
+        try {
+        File file = File.createTempFile("realhowto", ".vbs");
+        file.deleteOnExit();
+        FileWriter fw = new java.io.FileWriter(file);
+
+
+        String vbs = "Set objFSO = CreateObject(\"Scripting.FileSystemObject\")\n"
+        + "Set colDrives = objFSO.Drives\n"
+        + "Set objDrive = colDrives.item(\""
+        + drive
+        + "\")\n"
+        + "Wscript.Echo objDrive.SerialNumber"; // see note
+        fw.write(vbs);
+        fw.close();
+        Process p = Runtime.getRuntime().exec(
+        "cscript //NoLogo " + file.getPath());
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+        p.getInputStream()));
+        String line;
+        while ((line = input.readLine()) != null) {
+        result += line;
+        }
+        input.close();
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        return result.trim();
+        }
+        
+        
+        /**
+        * 获取主板序列号
+        * 
+        * @return
+        */
+        public static String getMotherboardSN() {
+        String result = "";
+        String os = getOsName();  
+        if (os.startsWith("Mac")) return "";
+        try {
+        File file = File.createTempFile("realhowto", ".vbs");
+        file.deleteOnExit();
+        FileWriter fw = new java.io.FileWriter(file);
+
+
+        String vbs = "Set objWMIService = GetObject(\"winmgmts:\\\\.\\root\\cimv2\")\n"
+        + "Set colItems = objWMIService.ExecQuery _ \n"
+        + "   (\"Select * from Win32_BaseBoard\") \n"
+        + "For Each objItem in colItems \n"
+        + "    Wscript.Echo objItem.SerialNumber \n"
+        + "    exit for  ' do the first cpu only! \n" + "Next \n";
+
+
+        fw.write(vbs);
+        fw.close();
+        Process p = Runtime.getRuntime().exec(
+        "cscript //NoLogo " + file.getPath());
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+        p.getInputStream()));
+        String line;
+        while ((line = input.readLine()) != null) {
+        result += line;
+        }
+        input.close();
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        return result.trim();
+        }
           
       
           
