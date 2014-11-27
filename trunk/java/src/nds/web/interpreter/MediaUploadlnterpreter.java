aefe31a015feedd9783934282c5629eb8a613320
pacozhhejm <pacozhhejm@126.com>
2014-11-27 15:00:43 +0800

    优化多图片上传功能

--- trunk/java/src/nds/web/interpreter/MediaUploadlnterpreter.java
+++ trunk/java/src/nds/web/interpreter/MediaUploadlnterpreter.java
@@ -22,48 +22,52 @@ public class MediaUploadlnterpreter implements ColumnInterpreter,java.io.Seriali
   	
   	@Override
   	public String parseValue(Object value, Locale locale) {
-
-  		if(value==null) return "";
-		String[] vals=((String) value).split("@");
-		if(vals.length==2){
-    		this.objid=Integer.parseInt(vals[0]);
-    		this.colid=Integer.parseInt(vals[1]);
-    		Column col=TableManager.getInstance().getColumn(this.colid);
-    		table=col.getTable();
-    		JSONObject jor=col.getJSONProps();
-    		String medcol = jor.optString("mediacolumn"); 
-    		int mediamax = jor.optInt("mediamax");
-    		String htmlli = null;
-    		for(int i=0;i<mediamax;i++)
-    		{
-    			if(i==0)
-    			{
-    				htmlli= "<li ><div class='media-upload'><img class='imgc' id='p"+(i+1)+"' src='' width='100' height='100'/>"+
-			      					"<span class='float-label'>×</span>"+
-			      					"</div></li>";
-    			}else{
-    				
-    				htmlli+="<li class='cli' style='float:left;'><div id='media-upload' class='media-upload'><img class='imgc' id='p"+(i+1)+"' src='' width='100' height='100'/>"+
-  					"<span class='float-label'>×</span>"+
-  					"</div></li>";
-    			}
-    		}
-    		String html ="<script type='text/javascript' src='/html/nds/oto/js/mediaupload.js'></script>"+
-	    				 "<div  class='uploadmedia-body'>" +
-		    				"<div id='button_attention'>"+
-		    					"<div id='upload_44444' class='uploadifive-button'></div>"+
-		    				"</div>"+
-		    				"<div id='media_position'>"+
-		    				    "<ul  id='imgList'>"+htmlli+"</ul>"+
-		    				"</div>"+
-		    				"<div id='media_attention'><span>提示：</span><br><br>" +
-		    						"<span>可以上传<strong class='bright' style='color:red;'>"+mediamax+"</strong>张图片，还能上传1张</span><br><br>" +
-		    						"<span>图片建议尺寸100*100</span>" +
-		    				"</div>"+
-	    				"</div>";
-    		
-    		return html;
-		}
+  		try{
+	  		if(value==null) return "";
+			String[] vals=((String) value).split("@");
+			if(vals.length==2){
+	    		this.objid=Integer.parseInt(vals[0]);
+	    		this.colid=Integer.parseInt(vals[1]);
+	    		Column col=TableManager.getInstance().getColumn(this.colid);
+	    		table=col.getTable();
+	    		JSONObject jor=col.getJSONProps();
+	    		String medcol = jor.optString("mediacolumn"); 
+	    		int mediamax = jor.optInt("mediamax");
+	    		String htmlli = null;
+	    		for(int i=0;i<mediamax;i++)
+	    		{
+	    			if(i==0)
+	    			{
+	    				htmlli= "<li ><div class='media-upload'><img class='imgc' id='p"+(i+1)+"' src='' width='100' height='100'/>"+
+				      					"<span class='float-label'>×</span>"+
+				      					"</div></li>";
+	    			}else{
+	    				
+	    				htmlli+="<li class='cli' style='float:left;'><div id='media-upload' class='media-upload'><img class='imgc' id='p"+(i+1)+"' src='' width='100' height='100'/>"+
+	  					"<span class='float-label'>×</span>"+
+	  					"</div></li>";
+	    			}
+	    		}
+	    		String html ="<script type='text/javascript' src='/html/nds/oto/js/mediaupload.js'></script>"+
+		    				 "<div  class='uploadmedia-body'>" +
+			    				"<div id='button_attention'>"+
+			    					"<div id='upload_44444' class='uploadifive-button'></div>"+
+			    				"</div>"+
+			    				"<div id='media_position'>"+
+			    				    "<ul  id='imgList'>"+htmlli+"</ul>"+
+			    				"</div>"+
+			    				"<div id='media_attention'><span>提示：</span><br><br>" +
+			    						"<span>最多可以上传<strong class='bright' style='color:red;'>"+mediamax+"</strong>张图片</span><br><br>" +
+			    						"<span>图片建议尺寸100*100</span>" +
+			    				"</div>"+
+		    				"</div>";
+	    		
+	    		return html;	
+			}
+  		}catch(Exception e){
+  			System.out.println("upload image error->");
+  			e.printStackTrace();
+  		}
   		return "";
   	}
   	@Override
