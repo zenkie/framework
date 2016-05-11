bf887e94cba1ac040b7d4730818990143e0d447f
jackrain <jackrsky@gmail.com>
2016-05-11 16:23:19 +0800

    千分位格式化

--- trunk/java/src/nds/util/Tools.java
+++ trunk/java/src/nds/util/Tools.java
@@ -58,6 +58,39 @@ public final class Tools {
 	    Pattern pattern = Pattern.compile("^[-\\+]?[.\\d]*$");    
 	    return pattern.matcher(String.valueOf(str)).matches();    
 	  }  
+	  
+	  
+	  
+	/**
+  	* 在数字型字符串千分位加逗号
+  	* @param str
+  	* @return
+  	*/
+  	public static String addComma(String str){
+  		boolean neg = false;
+  		if (str.startsWith("-")){  //处理负数
+  			str = str.substring(1);
+  			neg = true;
+  		}
+  		String tail = null;
+  		if (str.indexOf('.') != -1){ //处理小数点
+  			tail = str.substring(str.indexOf('.'));
+  			str = str.substring(0, str.indexOf('.'));
+  		}
+  		StringBuilder sb = new StringBuilder(str);
+  		sb.reverse();
+  		for (int i = 3; i < sb.length(); i += 4){
+  			sb.insert(i, ',');
+  		}
+  		sb.reverse();
+  		if (neg){
+  			sb.insert(0, '-');
+  		}
+  		if (tail != null){
+  			sb.append(tail);
+  		}
+  		return sb.toString();
+  	}
 	
 	public static String nvl(String str) {  
         if(str == null || "null".equals(str)) {  
