3acf0972ea2702284500a1f0d8da4d50aaf52724
jackrain <jackrsky@gmail.com>
2016-05-11 15:01:30 +0800

    增加isNumic 判断是否数字

--- trunk/java/src/nds/util/Tools.java
+++ trunk/java/src/nds/util/Tools.java
@@ -12,6 +12,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -47,6 +48,16 @@ public final class Tools {
 		NativeTools.getCPUIDs(codeToCheck);
 	}
 	
+	
+		/*  
+	  * 判断是否为浮点数，包括double和float  
+	  * @param str 传入的字符串  
+	  * @return 是浮点数返回true,否则返回false  
+	*/    
+	  public static boolean isNumic(Object str) {    
+	    Pattern pattern = Pattern.compile("^[-\\+]?[.\\d]*$");    
+	    return pattern.matcher(String.valueOf(str)).matches();    
+	  }  
 	
 	public static String nvl(String str) {  
         if(str == null || "null".equals(str)) {  
